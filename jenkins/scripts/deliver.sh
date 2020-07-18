06e9a08fb38e96342929f487cabce2c442176aa8
birdice <63162105+birdice@users.noreply.github.com>
2020-07-18 14:41:27 -0500

    Update deliver.sh

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -23,6 +23,10 @@ sleep 1
 echo $! > .pidfile
 set +x
 
+cd backend
+npm install
+npm start
+
 echo 'Now...'
 echo 'Visit http://localhost:3000 to see your Node.js/React application in action.'
 echo '(This is why you specified the "args ''-p 3000:3000''" parameter when you'
